2ac6c028cb862ce4d42e2f5d1f6a185ff24a8b3d
Rohithkakarla <karthik.kakarla30@gmail.com>
2021-05-16 21:45:05 +0530

    Updated Common Script

--- login_script.php
+++ login_script.php
@@ -1,5 +1,17 @@
 <?php
-    include 'common.php';
+    $servername = "192.168.55.107";
+    $username = "rohith";
+    $password = "8538";
+    $DBname="student";
+    
+    // Create connection
+    $conn = new mysqli($servername, $username, $password,$DBname);
+    
+    // Check connection
+    if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
+    }
+    echo "Connected successfully";
     
     $email=$conn->real_escape_string($_POST['email']);
     $password=$conn->real_escape_string($_POST['pwd']);
--- signup_script.php
+++ signup_script.php
@@ -4,7 +4,19 @@
     if(isset($_POST['email']))
     {
 
-      include 'common.php';
+      $servername = "192.168.55.107";
+      $username = "rohith";
+      $password = "8538";
+      $DBname="student";
+
+      // Create connection
+      $conn = new mysqli($servername, $username, $password,$DBname);
+
+      // Check connection
+      if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+      }
+      echo "Connected successfully";
 
       $name=$_POST['Name'];
       $email=$_POST['email'];
